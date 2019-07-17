ce39164e762639870481cf4fdd4f46ff81078e7d
SanjitKal <sanjitkal@gmail.com>
2019-07-17 12:03:19 -0700

    generate parser for super barebones SQL wit grammer specification

--- db-engine/parser/parse.go
+++ db-engine/parser/parse.go
@@ -1,20 +1,47 @@
-package parse
+package parser
+
+//Relavant parser definitions and functions needed to define a simple SQL parser
 
 import (
-	"strings"
+	"errors"
+
+	"github.com/alecthomas/participle"
 )
 
-type Stmt interface {
-}
-
+/*SelectStmt -
+ */
 type SelectStmt struct {
-	Stmt
-	tablenames []string
-	colnames   []string
+	ColNames   []*ColName   `"SELECT" (@@)+ "FROM"`
+	TableNames []*TableName `(@@)+`
 	// TODO: Handle WHERE clause
 }
 
-func sqlCommand(sqlString string) SelectStmt {
-	// Assumption: the command is a SELECT commands
-	strings.SplitAfterN(sqlString, "SELECT", 2)
+/*ColName -
+ */
+type ColName struct {
+	ColName string `@Ident","`
+}
+
+/*TableName -
+ */
+type TableName struct {
+	TableName string `@Ident","`
+}
+
+var SqlParser *participle.Parser
+
+/*InitParser -
+Creates a parser with the simple SQL grammar defined above
+*/
+func InitParser() error {
+
+	parser, parseErr := participle.Build(&SelectStmt{})
+
+	if parseErr != nil {
+		parseErr := errors.New("Error creating parser:" + parseErr.Error())
+		return parseErr
+	}
+
+	SqlParser = parser
+	return nil
 }
